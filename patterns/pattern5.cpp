3eed54cffd71aa8306815438db481fa834f665d2
Tanmay Srivastava <tanmaysrivastava12415@gmail.com>
2026-09-06 16:36:50 +0530

    Add C++ pattern programs

---
+++ patterns/pattern5.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern5(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<n-i+1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    pattern5(n);
+}
---
+++ patterns/pattern8.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern8(int n){
+    for(int i=0;i<n;i++){
+        // space
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        // stars
+        for(int j=0;j<2*n-(2*i+1);j++){
+            cout<<"*";
+            }
+        // space
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    pattern8(n);
+}
