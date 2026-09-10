892c4eafa0c9c0db4bce6965c06dc2a3e74119c4
Tanmay Srivastava <tanmaysrivastava12415@gmail.com>
2026-09-10 19:54:13 +0530

    Added patterns 20 to 22

---
+++ patterns/pattern20.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern20(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        for(int j=1;j<=2*(n-i);j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=1;i<=n;i++){ 
+        for(int j=1;j<=i;j++){ 
+            cout<<"*"; 
+        } 
+        for(int j=1;j<=2*(n-i);j++){ 
+            cout<<" "; 
+        } 
+        for(int j=1;j<=i;j++){ 
+            cout<<"*"; 
+        } 
+        cout<<endl; 
+    }
+    for(int i = n - 1; i >= 1; i--){ 
+        for(int j = 1; j <= i; j++){ 
+            cout << "*"; 
+        } 
+        for(int j = 1; j <= 2 * (n - i); j++){ 
+            cout << " "; 
+        } 
+        for(int j = 1; j <= i; j++){ 
+            cout << "*"; 
+        } 
+        cout << endl; 
+    } 
+}
+int main(){
+    int n;
+    cin>>n;
+    pattern20(n);
+}
